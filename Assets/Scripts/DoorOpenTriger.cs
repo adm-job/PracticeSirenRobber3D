d3f1964f4daae79f748b9a57697565a8cbc37398
Stanislav <adm.job@yandex.ru>
2026-03-10 02:12:08 +0500

    add animation

---
+++ Assets/Scripts/DoorOpenTriger.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpenTriger : MonoBehaviour
+{
+    [SerializeField] private Door _door;
+
+    private bool _isOpened = false;
+    private bool _hasOpener;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(_hasOpener);
+        if (other.TryGetComponent<DoorOpener>(out _))
+        {
+            _hasOpener = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log(_hasOpener);
+        if (other.TryGetComponent<DoorOpener>(out _))
+        {
+            _hasOpener = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (_isOpened)
+        {
+            return;
+        }
+
+        if (_hasOpener && Input.GetKeyDown(KeyCode.E))
+        {
+            _door.Open();
+            _isOpened = false;
+        }
+
+    }
+}
