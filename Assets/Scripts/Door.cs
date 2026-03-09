cda6fa6841fad520bc2787c3500c6f89bc9eff76
Stanislav <adm.job@yandex.ru>
2026-03-10 01:03:18 +0500

    Update Door.cs

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private readonly int OpenTriger = Animator.StringToHash("Open");
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private Animator _animator;
+
+    public void Open()
     {
-        
+        _animator.SetTrigger(OpenTriger);
     }
 }
